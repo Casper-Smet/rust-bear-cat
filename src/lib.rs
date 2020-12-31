4ff5e548090e177b5bf219db54a56b318fad277a
Casper Smet <casper.smet@student.hu.nl>
2020-12-31 16:17:32 +0100

    Two variable lin reg

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod regression;
+
+#[test]
+fn reg() {
+    let x = 30.0;
+    let a = 65.14157;
+    let b = 0.385225;
+    let y = a + x * b;
+
+    println!("{} + {} * {} = {}", a, b, x, y);
+
+    let age: Vec<f32> = vec![43.0, 21.0, 25.0, 42.0, 57.0, 59.0];
+    let glucose: Vec<f32> = vec![99.0, 65.0, 79.0, 75.0, 87.0, 81.0];
+    let func = regression::linear_regression(&age, &glucose);
+    let y_test = func(x);
+    println!("{}", y_test);
+    assert_eq!(y, y_test);
+    
+    let age: [f32; 6] = [43.0, 21.0, 25.0, 42.0, 57.0, 59.0];
+    let glucose: [f32; 6] = [99.0, 65.0, 79.0, 75.0, 87.0, 81.0];
+    let func = regression::linear_regression(&age, &glucose);
+    let y_test = func(x);
+    println!("{}", y_test);
+    assert_eq!(y, y_test);
 }
---
+++ src/regression.rs
@@ -0,0 +1,27 @@
+/// Linear regression on one variable.
+pub fn linear_regression(x_vec: &[f32], y: &[f32]) ->  impl Fn(f32) -> f32 {
+    
+    assert_eq!(x_vec.len(), y.len());
+
+    let n: f32 = x_vec.len() as f32;
+    let mut xy_sum: f32 = 0.0;
+    for it in x_vec.iter().zip(y.iter()) {
+        let (_x, _y) = it;
+        xy_sum += _x * _y;
+    }
+
+    let y_sum: f32 = y.iter().sum();
+    let x_square: f32 = x_vec.iter().fold(0.0, |acc, x| acc + x.powf(2.0));
+    let x_sum: f32 = x_vec.iter().sum();
+
+
+    let a_num: f32 = y_sum * x_square - x_sum * xy_sum;
+    let a_den: f32 = n * x_square - x_sum.powf(2.0);
+    let a: f32 = a_num / a_den;
+
+    let b_num: f32 = n * xy_sum - x_sum * y_sum;
+    let b_den: f32 = n * x_square - x_sum.powf(2.0);
+    let b: f32 = b_num / b_den;
+    
+    move |x: f32| {a + b * x}
+}
