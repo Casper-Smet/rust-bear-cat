09dc6ad4a799213c74d4fd1da78c4becd9c0631a
Casper Smet <casper.smet@student.hu.nl>
2021-01-02 14:18:00 +0100

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+mod classification;
 #[cfg(test)]
 mod regression;
-mod classification;
 
 #[test]
 fn reg() {
@@ -17,7 +17,6 @@ fn reg() {
     let y_test = func(x);
     println!("{}", y_test);
     assert_eq!(y, y_test);
-    
     let age: [f32; 6] = [43.0, 21.0, 25.0, 42.0, 57.0, 59.0];
     let glucose: [f32; 6] = [99.0, 65.0, 79.0, 75.0, 87.0, 81.0];
     let func = regression::linear_regression(&age, &glucose);
@@ -29,7 +28,12 @@ fn reg() {
 fn knn() {
     let k = 2;
     let x: [[f32; 2]; 4] = [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [0.06, 7.0]];
-    let train: [[f32; 3]; 4] = [[0.0, 0.0, 0.0], [0.5, 0.5, 0.0], [3.0, 3.0, 1.0], [4.0, 3.0, 1.0]];
+    let train: [[f32; 3]; 4] = [
+        [0.0, 0.0, 0.0],
+        [0.5, 0.5, 0.0],
+        [3.0, 3.0, 1.0],
+        [4.0, 3.0, 1.0],
+    ];
     let y_test: Vec<f32> = classification::k_nearest_neighbours(k, &x, &train);
     let y_true: Vec<f32> = vec![0.0, 1.0, 1.0, 1.0];
     assert_eq!(y_test, y_true);
