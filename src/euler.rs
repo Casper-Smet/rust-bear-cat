aecd80b0e45c981cf71aeaeeb191ffd19fc6636e
Casper Smet <casper.smet@student.hu.nl>
2021-03-11 22:21:56 +0100

    Added new euler approx

--- src/euler.rs
+++ src/euler.rs
@@ -1,6 +1,22 @@
 use crate::math::factorial;
+use rand::Rng;
 
 pub fn e_approximation(n: u128) -> f64 {
     // Approximation of Eulers number
     (0..n).fold(0.0, |a, b| a + 1. / factorial(b) as f64)
+}
+
+fn just_once() -> f64 {
+    let mut counter: f64 = 0.;
+    let mut summer: f64 = 0.;
+    let mut rng = rand::thread_rng();
+    while summer <= 1. {
+        summer += rng.gen::<f64>();
+        counter += 1.;
+    }
+    counter
+}
+
+pub fn random_e(n: u128) -> f64 {
+    (0..n).map(|_| just_once()).sum::<f64>() / (n as f64)
 }
