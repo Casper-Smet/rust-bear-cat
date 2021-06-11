8d2fe07bf43b13c731b50e72ffd7b1e17f4df502
Denbergvanthijs <denbergvanthijs@gmail.com>
2021-06-11 11:24:35 +0200

    Maybe(working) -> Just Nothing

--- src/neuron.rs
+++ src/neuron.rs
@@ -3,10 +3,28 @@ pub struct Node {
     pub weights: Vec<f32>,
     pub bias: f32,
     pub learning_rate: f32,
+    output: f32,
+    error: f32,
+    delta_weights: Vec<f32>,
+    delta_bias: f32,
+    gradients: Vec<f32>,
 }
 
 impl Node {
-    pub fn activate(&self, input: &Vec<f32>) -> f32 {
+    pub fn new(weights: Vec<f32>, bias: f32, learning_rate: f32) -> Node {
+        Node {
+            weights: weights,
+            bias: bias,
+            learning_rate: learning_rate,
+            output: 0.,
+            error: 0.,
+            delta_weights: Vec::new(),
+            delta_bias: 0.,
+            gradients: Vec::new(),
+        }
+    }
+
+    pub fn activate(&mut self, input: &Vec<f32>) -> f32 {
         if input.len() != self.weights.len() {
             panic!(
                 "Input and weights have different lengths, got {} and {}",
@@ -14,21 +32,24 @@ impl Node {
                 self.weights.len()
             );
         }
-        let total = self.weights.iter().zip(input.iter()).map(|(a, b)| a * b).sum::<f32>() + self.bias;
-        self.sigmoid(&total)
+        self.output = self.weights.iter().zip(input.iter()).map(|(a, b)| a * b).sum::<f32>() + self.bias;
+        self.output = self.sigmoid(&self.output);
+        self.output
     }
 
     pub fn sigmoid(&self, weighted_sum: &f32) -> f32 {
         1. / (1. + std::f32::consts::E.powf(-weighted_sum))
     }
+
     pub fn d_sigmoid(&self, output: &f32) -> f32 {
         output * (1. - output)
     }
 
-    pub fn error_output(&self, output: &f32, target: &f32) -> f32 {
-        self.d_sigmoid(output) * -(target - output)
+    pub fn error_output(&mut self, target: &f32) {
+        self.error = self.d_sigmoid(&self.output) * -(target - &self.output);
     }
-    pub fn error_hidden(&self, output: &f32, next_weights: &Vec<f32>, next_errors: &Vec<f32>) -> f32 {
+
+    pub fn error_hidden(&mut self, next_weights: &Vec<f32>, next_errors: &Vec<f32>) {
         if next_weights.len() != next_errors.len() {
             panic!(
                 "Weights and errors have different lengths, got {} and {}",
@@ -36,101 +57,97 @@ impl Node {
                 next_errors.len()
             );
         }
-        output * next_weights.iter().zip(next_errors).map(|(w, e)| w * e).sum::<f32>()
+        self.error = &self.output * (1. - &self.output) * next_weights.iter().zip(next_errors).map(|(w, e)| w * e).sum::<f32>();
     }
 
-    pub fn calculate_delta_weights(&self, inputs: &Vec<f32>, error: &f32) -> Vec<f32> {
-        inputs.iter().map(|w| self.learning_rate * error * w).collect()
+    pub fn calculate_gradients(&mut self, prev_outputs: &Vec<f32>) {
+        if prev_outputs.len() != self.weights.len() {
+            panic!(
+                "Previous outputs and current weights have different lengths, got {} and {}",
+                prev_outputs.len(),
+                self.weights.len()
+            );
+        }
+        self.gradients = prev_outputs.iter().map(|o| self.error * o).collect()
     }
 
-    pub fn calculate_delta_bias(&self, error: &f32) -> f32 {
-        self.learning_rate * error
+    pub fn calculate_delta_weights(&mut self) {
+        if self.weights.len() != self.gradients.len() {
+            panic!(
+                "Weights and gradients have different lengths, got {} and {}",
+                self.weights.len(),
+                self.gradients.len()
+            );
+        }
+        self.delta_weights = self.gradients.iter().map(|g| &self.learning_rate * g).collect();
     }
 
-    pub fn update_weights(&mut self, delta_weights: &Vec<f32>) {
-        if self.weights.len() != delta_weights.len() {
+    pub fn calculate_delta_bias(&mut self) {
+        self.delta_bias = &self.learning_rate * &self.error;
+    }
+
+    pub fn update_weights(&mut self) {
+        if self.weights.len() != self.delta_weights.len() {
             panic!(
                 "Weights and delta weights have different lengths, got {} and {}",
                 self.weights.len(),
-                delta_weights.len()
+                self.delta_weights.len()
             );
         }
-        self.weights = self.weights.iter().zip(delta_weights).map(|(w, d)| w - d).collect();
+        self.weights = self.weights.iter().zip(&self.delta_weights).map(|(w, d)| w - d).collect();
     }
 
-    pub fn update_bias(&mut self, delta_bias: &f32) {
-        self.bias = self.bias - delta_bias;
-    }
-
-    // MSE Loss
-    pub fn loss(&mut self, inputs: &Vec<&Vec<f32>>, targets: &Vec<f32>) -> f32 {
-        if inputs.len() != targets.len() {
-            panic!(
-                "Inputs and targets have different lengths, got {} and {}",
-                inputs.len(),
-                targets.len()
-            );
-        }
-        let outputs: Vec<f32> = inputs.iter().map(|i| self.activate(i)).collect();
-        outputs
-            .iter()
-            .zip(targets)
-            .map(|(i, t)| self.error_output(&i, t).powf(2.))
-            .sum::<f32>()
-            / targets.len() as f32
-    }
-
-    pub fn epoch(&mut self, inputs: &Vec<&Vec<f32>>, targets: &Vec<f32>) -> f32 {
-        if inputs.len() != targets.len() {
-            panic!(
-                "Inputs and targets have different lengths, got {} and {}",
-                inputs.len(),
-                targets.len()
-            );
-        }
-        for (input, target) in inputs.iter().zip(targets) {
-            let output = self.activate(input);
-            let error = self.error_output(&output, target);
-            let delta_weights = self.calculate_delta_weights(input, &error);
-            let delta_bias = self.calculate_delta_bias(&error);
-            self.update_weights(&delta_weights);
-            self.update_bias(&delta_bias);
-        }
-        self.loss(inputs, targets)
+    pub fn update_bias(&mut self) {
+        self.bias -= self.delta_bias;
     }
 }
 
 pub struct Layer {
     pub nodes: Vec<Node>,
+    activations: Vec<f32>,
+    errors: Vec<f32>,
 }
 
 impl Layer {
-    pub fn activate(&self, input: &Vec<f32>) -> Vec<f32> {
-        self.nodes.iter().map(|n| n.activate(input)).collect()
-    }
-    pub fn weights(&self) -> Vec<&Vec<f32>> {
-        self.nodes.iter().map(|n| &n.weights).collect()
+    pub fn new(nodes: Vec<Node>) -> Layer {
+        Layer {
+            nodes: nodes,
+            activations: Vec::new(),
+            errors: Vec::new(),
+        }
     }
 
-    pub fn errors_output(&self, output: &Vec<f32>, target: &Vec<f32>) -> Vec<f32> {
-        if output.len() != target.len() {
-            panic!("Input and target have different lengths, got {} and {}", output.len(), target.len());
+    pub fn activate(&mut self, input: &Vec<f32>) -> &Vec<f32> {
+        for n in self.nodes.iter() {
+            if n.weights.len() != input.len() {
+                panic!(
+                    "Node weights and inputs have different lengths, got {} and {}",
+                    n.weights.len(),
+                    input.len()
+                );
+            }
         }
-        self.nodes
-            .iter()
-            .zip(output)
-            .zip(target)
-            .map(|((n, o), t)| n.error_output(&o, t))
-            .collect()
+        self.activations = self.nodes.iter_mut().map(|n| n.activate(input)).collect::<Vec<f32>>();
+        &self.activations
     }
-    pub fn errors_hidden(&self, output: &Vec<f32>, weights: &Vec<&Vec<f32>>, errors: &Vec<f32>) -> Vec<f32> {
-        if self.nodes.len() != output.len() {
+
+    pub fn weights(&self) -> Vec<Vec<f32>> {
+        self.nodes.iter().map(|n| n.weights.clone()).collect()
+    }
+
+    pub fn errors_output(&mut self, target: &Vec<f32>) {
+        if self.nodes.len() != target.len() {
             panic!(
-                "Number of nodes is not equal to length of output, got {} and {}",
+                "Input and target have different lengths, got {} and {}",
                 self.nodes.len(),
-                output.len()
+                target.len()
             );
         }
+        self.nodes.iter_mut().zip(target).for_each(|(n, t)| n.error_output(t));
+        self.errors = self.nodes.iter().map(|n| n.error).collect();
+    }
+
+    pub fn errors_hidden(&mut self, weights: &Vec<Vec<f32>>, errors: &Vec<f32>) {
         for w in weights {
             if self.nodes.len() != w.len() {
                 panic!(
@@ -141,54 +158,74 @@ impl Layer {
             }
         }
         self.nodes
-            .iter()
-            .zip(output)
+            .iter_mut()
             .enumerate()
-            .map(|(i, (n, o))| n.error_hidden(&o, &weights.iter().map(|k| k[i]).collect::<Vec<f32>>(), &errors))
-            .collect()
+            .for_each(|(i, n)| n.error_hidden(&weights.iter().map(|k| k[i]).collect::<Vec<f32>>(), &errors));
+        self.errors = self.nodes.iter().map(|n| n.error).collect();
+    }
+
+    pub fn calculcate_gradients(&mut self, prev_outputs: &Vec<f32>) {
+        for n in self.nodes.iter() {
+            if n.weights.len() != prev_outputs.len() {
+                panic!(
+                    "Node weights and previous outputs have different lengths, got {} and {}",
+                    n.weights.len(),
+                    prev_outputs.len()
+                );
+            }
+        }
+        self.nodes.iter_mut().for_each(|n| n.calculate_gradients(prev_outputs));
     }
 }
 
 pub struct Network {
     pub layers: Vec<Layer>,
+    output: Vec<f32>,
 }
 
 impl Network {
-    pub fn activate(&self, input: &Vec<f32>) -> Vec<f32> {
-        let mut output = input.to_vec();
-        for l in self.layers.iter() {
+    pub fn new(layers: Vec<Layer>) -> Network {
+        Network {
+            layers: layers,
+            output: Vec::new(),
+        }
+    }
+
+    pub fn activate(&mut self, input: &Vec<f32>) -> &Vec<f32> {
+        let mut output = input;
+        for l in self.layers.iter_mut() {
             output = l.activate(&output);
         }
-        output
-    }
-    pub fn activate_full(&self, input: &Vec<f32>) -> Vec<Vec<f32>> {
-        let mut outputs = vec![input.to_vec()];
-        for (i, l) in self.layers.iter().enumerate() {
-            outputs.push(l.activate(&outputs[i]));
-        }
-        outputs
+        self.output = output.to_vec();
+        &self.output
     }
 
     pub fn backprop(&mut self, input: &Vec<f32>, target: &Vec<f32>) {
-        let output = self.activate(input);
-        let activations = self.activate_full(&input);
-        &self.layers.reverse();
-        let weights = &self.layers.iter().map(|l| l.weights()).collect::<Vec<Vec<&Vec<f32>>>>();
-        let mut errors = vec![self.layers[0].errors_output(&output, &target)];
+        for i in (0..self.layers.len()).rev() {
+            let prev_output = if i == 0 {
+                input.clone()
+            } else {
+                self.layers[i - 1].activations.clone()
+            };
+            if i == self.layers.len() - 1 {
+                self.layers[i].errors_output(&target);
+            } else {
+                let next_weights = self.layers[i + 1].weights().clone();
+                let next_errors = self.layers[i + 1].errors.clone();
 
-        for i in 1..self.layers.len() {
-            errors.push(self.layers[i].errors_hidden(&activations[i], &weights[i - 1], &errors[i - 1]));
+                self.layers[i].errors_hidden(&next_weights, &next_errors);
+            }
+            self.layers[i].calculcate_gradients(&prev_output);
         }
 
         for layer in 0..self.layers.len() {
             for node in 0..self.layers[layer].nodes.len() {
-                let delta_weights = self.layers[layer].nodes[node].calculate_delta_weights(input, &errors[layer][node]);
-                let delta_bias = self.layers[layer].nodes[node].calculate_delta_bias(&errors[layer][node]);
-                self.layers[layer].nodes[node].update_weights(&delta_weights);
-                self.layers[layer].nodes[node].update_bias(&delta_bias);
+                self.layers[layer].nodes[node].calculate_delta_weights();
+                self.layers[layer].nodes[node].calculate_delta_bias();
+                self.layers[layer].nodes[node].update_weights();
+                self.layers[layer].nodes[node].update_bias();
             }
         }
-        &self.layers.reverse(); // Restore inplace reverse
     }
 
     pub fn epoch(&mut self, inputs: &Vec<&Vec<f32>>, targets: &Vec<&Vec<f32>>) {
@@ -199,10 +236,12 @@ impl Network {
                 targets.len()
             );
         }
-        for (input, target) in inputs.iter().zip(targets) {
+        for (target, input) in targets.iter().zip(inputs) {
+            self.activate(input);
             self.backprop(input, target);
         }
     }
+
     pub fn train(&mut self, inputs: &Vec<&Vec<f32>>, targets: &Vec<&Vec<f32>>, epochs: u128) {
         for _ in 0..epochs {
             self.epoch(inputs, targets);
